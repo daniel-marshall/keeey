35492f88a871e360285485796b33a8cb14ed9e8a
Daniel Marshall <d.marshall@outlook.com>
2025-02-19 20:40:14 +0000

    Gradle Build Integration

--- lib/keeey-stack.ts
+++ lib/keeey-stack.ts
@@ -1,6 +1,9 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import * as pipelines from 'aws-cdk-lib/pipelines';
+import * as codepipeline from 'aws-cdk-lib/aws-codepipeline';
+import * as codepipeline_actions from 'aws-cdk-lib/aws-codepipeline-actions';
+import * as codebuild from 'aws-cdk-lib/aws-codebuild';
 
 export class KeeeyPipelineStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
@@ -16,10 +19,35 @@ export class KeeeyPipelineStack extends cdk.Stack {
           {
             connectionArn:
               'arn:aws:codeconnections:us-east-1:794038246157:connection/ea3d36ad-20ec-4d43-8a71-6507352dcda8', // Created using the AWS console
+            triggerOnPush: true,
           }
         ),
         commands: ['npm ci', 'npm run build', 'npx cdk synth'],
       }),
+      selfMutation: false
     });
+
+    const stackScope = this;
+    pipeline.addWave('LambdaBuild',
+      {
+        post: [
+          new pipelines.CodeBuildStep('LambdaBuildStep', {
+            commands: ['./gradlew docker-publish -Paccount_id=794038246157 -Pregion=us-east-1 -Prepo_name=hello'],
+            buildEnvironment: {
+              buildImage: codebuild.LinuxBuildImage.AMAZON_LINUX_2023_5,
+            },
+            input: pipelines.CodePipelineSource.connection(
+              'daniel-marshall/keeey-lambda',
+              'main',
+              {
+                connectionArn:
+                  'arn:aws:codeconnections:us-east-1:794038246157:connection/ea3d36ad-20ec-4d43-8a71-6507352dcda8', // Created using the AWS console
+                triggerOnPush: true,
+              }
+            ),
+          }),
+        ],
+      }
+    );
   }
 }
