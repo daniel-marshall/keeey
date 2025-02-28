0dabf79815eca1e91e1ef65157d90816ffd168cb
Daniel Marshall <d.marshall@outlook.com>
2025-02-28 15:19:06 +0000

    Read Image from Env

--- lib/keeey-stack.ts
+++ lib/keeey-stack.ts
@@ -4,14 +4,15 @@ import * as lambda from 'aws-cdk-lib/aws-lambda';
 import { Repository } from 'aws-cdk-lib/aws-ecr';
 
 export interface Props extends cdk.StackProps {
-  ecrRepoName: string
+  ecrRepo: cdk.CfnOutput,
+  ecrRepoEnvVarName: string
 }
 
 export class KeeeyStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props: Props) {
     super(scope, id, props);
-    const ecrDigest = new cdk.CfnParameter(this, 'propertyfrombuild').valueAsString;
-    const repo = Repository.fromRepositoryName(this, 'ImageSource', props.ecrRepoName);
+    const ecrDigest = process.env[props.ecrRepoEnvVarName];
+    const repo = Repository.fromRepositoryName(this, 'ImageSource', props.ecrRepo.importValue);
     new lambda.Function(this, 'Function', {
       runtime: lambda.Runtime.FROM_IMAGE,
       handler: lambda.Handler.FROM_IMAGE,
